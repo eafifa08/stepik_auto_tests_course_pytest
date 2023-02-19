1fb16e0cb43656e7d8e37534ab3eece54b2f2de4
neo <eafifa07@yandex.ru>
2023-02-19 11:28:43 +0500

    time 3->30

--- test_items.py
+++ test_items.py
@@ -8,7 +8,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 class TestSite:
     def test_is_basket_button_exist(self, browser):
         browser.get(link)
-        time.sleep(3)
+        time.sleep(30)
         el_button_basket = browser.find_elements(By.CLASS_NAME,'btn-add-to-basket')
         print(f'button text={el_button_basket[0].text}')
         assert len(el_button_basket) > 0, 'Button "add to basket" is not exist'
